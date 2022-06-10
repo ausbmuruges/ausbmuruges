eab08543547704bc6739d84805df15fad0f57d99
ausbmuruges <ausbmuruges>
2022-06-10 09:17:59 +0000

    Updated Mount Script

--- ADB/Mount.py
+++ ADB/Mount.py
@@ -1,45 +1,5 @@
 # Databricks notebook source
-env='prd'
-stg_store_keys = dbutils.secrets.get(scope="stgstore", key="anausim{}stg01-primary".format(env))
-stg_store_keys
-
-# COMMAND ----------
-
-exisiting_mount_points = [mount_point.mountPoint for mount_point in dbutils.fs.mounts()]
-exisiting_mount_points
-
-# COMMAND ----------
-
-dbutils.fs.mounts()
-
-# COMMAND ----------
-
-env = "prd"
-streaming_mount_path = "/mnt/streaming"
-
-# COMMAND ----------
-
-if streaming_mount_path not in exisiting_mount_points:
-  dbutils.fs.mount(
-    source = "wasbs://streaming@anausim{}stg01.blob.core.windows.net".format(env),
-    mount_point = streaming_mount_path,
-    extra_configs = {"fs.azure.account.key.anausim{}stg01.blob.core.windows.net".format(env) : stg_store_keys})
-
-# COMMAND ----------
-
-dbutils.fs.mounts()
-
-# COMMAND ----------
-
-dbutils.fs.ls('dbfs:/mnt/streaming/')
-
-# COMMAND ----------
-
-# dbutils.fs.rm('dbfs:/mnt/streaming/logs',True)
-
-# COMMAND ----------
-
-dbutils.fs.ls('/mnt/streaming')
+display(dbutils.fs.mounts())
 
 # COMMAND ----------
 
@@ -47,27 +7,49 @@ display(dbutils.fs.mounts())
 
 # COMMAND ----------
 
+env = "dev"
+mount_path = "/mnt/imrv"
+store_key = dbutils.secrets.get(scope="rvstore", key="anausim{}rv01-primary".format(env))
+
+# COMMAND ----------
+
+# Mount a new path for RV
+dbutils.fs.mount(
+    source = "wasbs://streaming@anausim{}stg01.blob.core.windows.net".format(env),
+    mount_point = mount_path,
+    extra_configs = {"fs.azure.account.key.anausim{}stg01.blob.core.windows.net".format(env) : store_key})
+
+# COMMAND ----------
+
+# Update a exisitng Mount path 
+dbutils.fs.updateMount(
+    source = "wasbs://raw@anausim{}rv01.blob.core.windows.net".format(env),
+    mount_point = mount_path,
+    extra_configs = {"fs.azure.account.key.anausim{}rv01.blob.core.windows.net".format(env) : store_key})
+
+# COMMAND ----------
+
+# dbutils.fs.rm('dbfs:/mnt/streaming/logs',True)
+
+# COMMAND ----------
+
+display(dbutils.fs.ls(mount_path+'/deltalake'))
+
+# COMMAND ----------
+
 dbutils.secrets.help()
 
 # COMMAND ----------
 
-dbutils.secrets.listScopes()
+display(dbutils.secrets.listScopes())
 
 # COMMAND ----------
 
-display(dbutils.secrets.list(scope = 'astimdevlake_scope'))
+display(dbutils.secrets.list(scope = 'rvstore'))
 
 # COMMAND ----------
 
-display(dbutils.secrets.list(scope = 'fsstore'))
-
-# COMMAND ----------
-
- dbutils.secrets.get(scope="fsstore", key="fsstore-storage-account")
-
-# COMMAND ----------
-
-value = dbutils.secrets.get(scope="fsstore", key="fsstore-keyvault")
+value = dbutils.secrets.get(scope="rvstore", key="anausimdevrv01-primary")
 
 print('keyvault')
 for char in value:
